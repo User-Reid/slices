06723a1c5490e423ef105765ee5be03644c1cf15
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-30 16:36:02 -0600

    understanding the length method and slicing strings by bytes, not by characters.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
-    let first_name = {
-    let action_hero: &str = "Arnold Banana";
-    &action_hero[0..6]
-    };
-
-    println!("{first_name}")
+    let food: &str = "🍕";
+    println!("{}", food.len());
+    let pizza_slice: &str = &food[0..4];
+    println!("{pizza_slice}")
 }
