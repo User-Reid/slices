794e9a8df6372eb66d653368b464c7e7d7874030
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-31 11:49:24 -0600

    working with deref coercion with array slices.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+fn print_length(x: &[i32]) {
+    println!("{}", x.len());
+}
+
 fn main() {
     let values: [i32; 6] = [4, 8, 15, 16, 23, 42];
 
-    let my_slice: &[i32] = &values[0..3];
+    let regular_array: &[i32; 6] = &values;
 
-    println!("{my_slice:?}");
+    let slice_of_three: &[i32] = &values[..3];
 
-    let my_slice: &[i32] = &values[2..4];
-    println!("{my_slice:?}")
+    print_length(&values);
 }
