6477301214d5301cd172edf840d8db2b99697123
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-31 09:46:14 -0600

    undestanding syntax shortcuts with slice range.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let food: &str = "🍕";
-    println!("{}", food.len());
-    let pizza_slice: &str = &food[0..4];
-    println!("{pizza_slice}")
+    let action_hero: String = String::from("Arnold Banana");
+
+    let first_name: &str = &action_hero[..6];
+    println!("{first_name}");
+
+    let last_name: &str = &action_hero[7..];
+    println!("{last_name}");
+
+    let full_name: &str = &action_hero[..];
 }
